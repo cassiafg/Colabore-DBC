b43a86e12acfa0bb75e2f8578ffaea4e758a3ab0
cassiafg <cassiafguimaraes@gmail.com>
2022-08-18 13:48:05 -0300

    testes backend - endpoints autenticacao com bugs

--- testes-backend/src/main/java/service/AutenticacaoService.java
+++ testes-backend/src/main/java/service/AutenticacaoService.java
@@ -1,35 +1,68 @@
 package service;
 
-import dto.UsuarioDTO;
+import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 import io.restassured.response.Response;
+import pojo.RegisterRequestBody;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
 public class AutenticacaoService {
 
     String baseuri = "https://colabore-dbc-api.herokuapp.com/autenticacao";
+    String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb2xhYm9yZS1hcGkiLCJqdGkiOjYsImlhdCI6MTY2MDc3NDA0NCwiZXhwIjoxNjYwODYwNDQ0fQ.1jFmXaBCHyNQWM4CvJ233-1ZVYxEpI-IoNuKwiM3vTY";
 
-    public UsuarioDTO registrarUsuarioComSucesso(String jsonBody) {
+    RegisterRequestBody registerRequestBody = new RegisterRequestBody();
+    Faker faker = new Faker();
+    private String nome = faker.name().firstName();
+    private String email = faker.name().username()+"@dbccompany.com.br";
+    private String emailError = faker.internet().emailAddress();
+    private String senha = "123";
+//    private java.io.File file = new java.io.File("src/test/resources/data/teste.jpg");
+
+
+    public Response registrarUsuarioComSucesso(RegisterRequestBody registerRequestBody) {
+        this.registerRequestBody.setNome(nome);
+        this.registerRequestBody.setEmail(email);
+        this.registerRequestBody.setSenha(senha);
         return given()
                 .contentType(ContentType.JSON)
                 .log().all()
-                .body(jsonBody)
+                .body(this.registerRequestBody)
                 .when()
-                .post(baseuri+"/registrar")
+                .post(baseuri+"/cadastrar")
                 .then()
                 .log().all()
                 .statusCode(200)
-                .extract().as(UsuarioDTO.class);
+                .extract().response();
     }
 
-    public Response registrarUsuarioSemEmailESenha(String jsonBody) {
+    public Response registrarUsuarioComEmailForaDoPadrao(RegisterRequestBody registerRequestBody) {
+        this.registerRequestBody.setNome(nome);
+        this.registerRequestBody.setEmail(emailError);
+        this.registerRequestBody.setSenha(senha);
+        return given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(this.registerRequestBody)
+                .when()
+                .post(baseuri+"/cadastrar")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .extract().response();
+    }
+
+    public Response registrarUsuarioSemNome(String jsonBody) {
         return given()
                 .contentType(ContentType.JSON)
                 .log().all()
                 .body(jsonBody)
                 .when()
-                .post(baseuri+"/registrar")
+                .post(baseuri+"/cadastrar")
                 .then()
                 .log().all()
                 .statusCode(400)
@@ -56,6 +89,58 @@ public class AutenticacaoService {
                 .body(jsonBody)
                 .when()
                 .post(baseuri+"/login")
+                .then()
+                .log().all()
+                .statusCode(403)
+                .extract().response();
+    }
+
+    public Response logarUsuarioComEmailInvalido(String jsonBody) {
+        return given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(jsonBody)
+                .when()
+                .post(baseuri+"/login")
+                .then()
+                .log().all()
+                .statusCode(403)
+                .extract().response();
+    }
+
+    public Response createFotoComSucesso() {
+        return given().header("Authorization", token)
+                .header(new Header("content-type", "multipart/form-data" ))
+                .multiPart("multipartFile",new File("src/test/resources/data/teste.jpg"), "image/jpeg")
+                .log().all()
+                .when()
+                .post(baseuri+"/cadastrarFoto")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().response();
+    }
+
+    public Response createFotoSemEnviarArquivo() {
+        return given().header("Authorization", token)
+                .header(new Header("content-type", "multipart/form-data" ))
+                .multiPart("multipartFile","empty")
+                .log().all()
+                .when()
+                .post(baseuri+"/cadastrarFoto")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .extract().response();
+    }
+
+    public Response createFotoSemAutorizacao() {
+        return given()
+                .header(new Header("content-type", "multipart/form-data" ))
+                .multiPart("multipartFile",new File("src/test/resources/data/teste.jpg"), "image/jpeg")
+                .log().all()
+                .when()
+                .post(baseuri+"/cadastrarFoto")
                 .then()
                 .log().all()
                 .statusCode(403)
--- testes-backend/src/test/java/AutenticacaoAceitacao.java
+++ testes-backend/src/test/java/AutenticacaoAceitacao.java
@@ -1,7 +1,7 @@
-import dto.UsuarioDTO;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pojo.RegisterRequestBody;
 import service.AutenticacaoService;
 
 import java.io.IOException;
@@ -12,42 +12,16 @@ public class AutenticacaoAceitacao {
 
     AutenticacaoService authService = new AutenticacaoService();
 
+    RegisterRequestBody registerRequestBody = new RegisterRequestBody();
+
     //Massa de dados para body
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    @Test  //POST - registrar um usuário
-    public void createUser() throws IOException {
-
-        //Caminho da massa
-        String jsonBody = lerJson("src/test/resources/data/usuario.json");
-
-        //POST - Chamada para o serviço
-        UsuarioDTO resultService = authService.registrarUsuarioComSucesso(jsonBody);
-
-        //Validações
-        Assert.assertEquals(resultService.getNome(), "teste");
-        Assert.assertEquals(resultService.getFoto(), "teste");
-    }
-
-    @Test  //POST - registrar um usuário sem senha
-    public void createUserSemSucesso() throws IOException {
-
-        //Caminho da massa
-        String jsonBody = lerJson("src/test/resources/data/usuario-sem-senha.json");
-
-        //POST - Chamada para o serviço
-        Response resultService = authService.registrarUsuarioSemEmailESenha(jsonBody);
-
-        //Validações
-        Assert.assertEquals(resultService.getStatusCode(), 400);
-    }
 
     @Test  //POST - login com sucesso
     public void logarComSucesso() throws IOException {
-        //Criar usuário para o teste
-        authService.registrarUsuarioComSucesso(lerJson("src/test/resources/data/usuario.json"));
 
         //Caminho da massa
         String jsonBody = lerJson("src/test/resources/data/login-com-sucesso.json");
@@ -67,6 +41,82 @@ public class AutenticacaoAceitacao {
 
         //POST - Chamada para o serviço
         Response resultService = authService.logarUsuarioComSenhaInvalida(jsonBody);
+
+        //Validações
+        Assert.assertEquals(resultService.getStatusCode(), 403);
+    }
+
+    @Test  //POST - login com e-mail inválido
+    public void logarComEmailInvalido() throws IOException {
+
+        //Caminho da massa
+        String jsonBody = lerJson("src/test/resources/data/login-email-invalido.json");
+
+        //POST - Chamada para o serviço
+        Response resultService = authService.logarUsuarioComSenhaInvalida(jsonBody);
+
+        //Validações
+        Assert.assertEquals(resultService.getStatusCode(), 403);
+    }
+
+    @Test  //POST - registrar um usuário
+    public void createUser() {
+
+        //POST - Chamada para o serviço
+        Response resultService = authService.registrarUsuarioComSucesso(registerRequestBody);
+
+        //Validações
+        Assert.assertEquals(resultService.getStatusCode(), 200);
+    }
+
+    @Test  //POST - registrar um usuário com e-mail fora do padrão
+    public void createUserEmailError() {
+
+        //POST - Chamada para o serviço
+        Response resultService = authService.registrarUsuarioComEmailForaDoPadrao(registerRequestBody);
+
+        //Validações
+        Assert.assertEquals(resultService.getStatusCode(), 400);
+    }
+
+    @Test  //POST - registrar um usuário sem nome
+    public void createUserSemSucesso() throws IOException {
+
+        //Caminho da massa
+        String jsonBody = lerJson("src/test/resources/data/usuario-sem-nome.json");
+
+        //POST - Chamada para o serviço
+        Response resultService = authService.registrarUsuarioSemNome(jsonBody);
+
+        //Validações
+        Assert.assertEquals(resultService.getStatusCode(), 400);
+    }
+
+    @Test  //POST - cadastrar foto para um usuário logado
+    public void createFotoComSucesso() {
+
+        //POST - Chamada para o serviço
+        Response resultService = authService.createFotoComSucesso();
+
+        //Validações
+        Assert.assertEquals(resultService.getStatusCode(), 200);
+    }
+
+    @Test  //POST - cadastrar foto para um usuário logado sem enviar o arquivo
+    public void createFotoSemEnviarArquivo() {
+
+        //POST - Chamada para o serviço
+        Response resultService = authService.createFotoSemEnviarArquivo();
+
+        //Validações
+        Assert.assertEquals(resultService.getStatusCode(), 400);
+    }
+
+    @Test  //POST - cadastrar foto para um usuário que não está logado
+    public void createFotoSemAutorizacao() {
+
+        //POST - Chamada para o serviço
+        Response resultService = authService.createFotoSemAutorizacao();
 
         //Validações
         Assert.assertEquals(resultService.getStatusCode(), 403);
